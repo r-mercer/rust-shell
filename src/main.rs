f54f655cc5b07783142f91d2f7e7567fa2f32d31
EdgarAllenExile <riley.mercer4@gmail.com>
2025-11-11 20:04:54 +0800

    Updates to codecrafters-shell-rust files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use pathsearch::find_executable_in_path;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
@@ -19,15 +20,17 @@ fn main() {
             let com = com_arr.next().unwrap_or_default();
             let term = com_arr.next().unwrap_or_default();
             match com {
-               "echo" => println!("{}", term),
-               "type" => {
-                   if builtins.contains(&term) {
+                "echo" => println!("{}", term),
+                "type" => {
+                    if builtins.contains(&term) {
                         println!("{} is a shell builtin", term)
-                   } else {
+                    } else if let Some(exe_path) = find_executable_in_path(&term) {
+                        println!("{} is {}", term, exe_path.display());
+                    } else {
                         println!("{}: not found", term)
-                   }
-               }
-               _ => {}
+                    }
+                }
+                _ => {}
             }
         } else {
             println!("{}: command not found", command.trim());
