9e9953e29db7cd96293a639a96820825dc0cb9fa
EdgarAllenExile <riley.mercer4@gmail.com>
2025-11-11 19:01:21 +0800

    does this want to circle back or exit?

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-// use std::string;
+// use std::string::StartsWith;
 
 fn main() {
     print!("$ ");
@@ -8,7 +8,26 @@ fn main() {
 
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    if command.trim() != "exit 0" {
+    // match command.trim() {
+    //     "exit 0" => (),
+    //     // Some(x) if x.starts => 
+    //     command.trim().starts_with("echo ") => 
+    //         "echo" => (),
+    //     _ => {
+    //         println!("{}: command not found", command.trim());
+    //         main();
+    //     }
+    // }
+
+    if command.trim() == "exit 0" {}
+    else if command.trim().contains(' ') {
+        let mut com_arr = command.trim().splitn(2, ' ');
+        if com_arr.next().unwrap() == "echo"
+        {
+            println!("{}", com_arr.next().unwrap() )
+        }
+    }
+    else {
         println!("{}: command not found", command.trim());
         main();
     }
