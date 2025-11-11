2d82f4dd47752b7667e1a56ecb6fa282da412a1e
EdgarAllenExile <riley.mercer4@gmail.com>
2025-11-11 19:17:12 +0800

    reformat to use while loop

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,24 @@ use std::io::{self, Write};
 // use std::string::StartsWith;
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    let mut exit = true;
 
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    // match command.trim() {
-    //     "exit 0" => (),
-    //     // Some(x) if x.starts => 
-    //     command.trim().starts_with("echo ") => 
-    //         "echo" => (),
-    //     _ => {
-    //         println!("{}: command not found", command.trim());
-    //         main();
-    //     }
-    // }
+    while exit {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    if command.trim() == "exit 0" {}
-    else if command.trim().contains(' ') {
-        let mut com_arr = command.trim().splitn(2, ' ');
-        if com_arr.next().unwrap() == "echo"
-        {
-            println!("{}", com_arr.next().unwrap() )
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+
+        if command.trim() == "exit 0" {
+            exit = false;
+        } else if command.trim().contains(' ') {
+            let mut com_arr = command.trim().splitn(2, ' ');
+            if com_arr.next().unwrap() == "echo" {
+                println!("{}", com_arr.next().unwrap())
+            }
+        } else {
+            println!("{}: command not found", command.trim());
         }
-    }
-    else {
-        println!("{}: command not found", command.trim());
-        main();
     }
 }
