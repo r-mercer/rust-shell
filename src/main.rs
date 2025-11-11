b33d453c3c7e95b5a6c20a2970fc159d3eb5f116
EdgarAllenExile <riley.mercer4@gmail.com>
2025-11-11 16:22:41 +0800

    accept input - reject all"

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+// use std::string;
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    println!("{}: command not found", command.trim());
 }
