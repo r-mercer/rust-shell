1046082b590c7d3070b4baa56bd6c45823ba4bc0
EdgarAllenExile <riley.mercer4@gmail.com>
2025-11-14 21:42:20 +0800

    did I handle this already

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -23,9 +23,9 @@ pub fn cd(path: &str) {
 fn get_path(path: &str) -> Result<PathBuf, Error> {
     let mut dest = PathBuf::from(path);
     if dest.is_relative() {
-        if dest.starts_with("./") {
-            dest.strip_prefix("./").expect("Issue with path prefix");
-        }
+        // if dest.starts_with("./") {
+        //     dest.strip_prefix("./").expect("Issue with path prefix");
+        // }
         let mut cwd = env::current_dir()?;
         while dest.starts_with("../") {
             dest.strip_prefix("../").expect("Issue with path prefix");
