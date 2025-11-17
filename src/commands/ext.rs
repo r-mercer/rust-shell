c195a833f308f9cae2e18e4bf5615c382e78e60b
EdgarAllenExile <riley.mercer4@gmail.com>
2025-11-18 06:34:03 +0800

    silly replace

--- src/commands/ext.rs
+++ src/commands/ext.rs
@@ -57,12 +57,14 @@ fn get_next_param(mut par: &str) -> (String, &str) {
     let ind = par.find(c).unwrap_or(par.len());
     let (a, b) = par.split_at(ind);
     let mut para = String::from(a);
-    para = para.replace("\"\"", "");
+    para = para.replace("\"\"", "").replace("''", "");
     let mut parb = b.strip_prefix(c).unwrap_or(b);
     if parb.starts_with(['\'', '"']) {
-        let (c, d) = get_next_param(parb);
-        para += c.as_str();
-        parb = d;
+        let (d, e) = get_next_param(parb);
+        para += d.as_str();
+        parb = e;
+        // } else {
+        //     parb = parb.trim();
     }
     (para, parb)
 }
