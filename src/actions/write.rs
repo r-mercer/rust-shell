86dbe7ab7d3e20fb5bf8604054619ec10f8309d8
r-mercer <riley.mercer4@gmail.com>
2026-02-04 12:57:57 +0800

    pretty basic prompt result

--- src/actions/write.rs
+++ src/actions/write.rs
@@ -5,16 +5,19 @@ use std::path::PathBuf;
 use crate::commands::command_type::{LineCommand, ResultCode};
 
 pub fn output_to(command: &LineCommand, result: &ResultCode) -> Result<()> {
-    let path = PathBuf::from(command.file_path.as_ref().unwrap());
-    fs::write(path, result.output_str.as_ref().unwrap().as_bytes())?;
+    if let Some(path_str) = command.file_path.as_ref() {
+        let path = PathBuf::from(path_str);
+        fs::write(path, result.output_str.as_ref().unwrap().as_bytes())?;
+    } else {
+        if let Some(s) = result.output_str.as_ref() {
+            println!("{}", s);
+        }
+    }
     Ok(())
 }
 
 pub fn output_to_vec(command: &LineCommand, result: &ResultCode) -> Result<()> {
-    let path = PathBuf::from(command.file_path.as_ref().unwrap());
     let mut buf = Vec::new();
-
-    // this should be if param is -1 then concat with tab
     if let Some(output) = result.output_vec.as_ref() {
         for token in output {
             if command
@@ -22,18 +25,17 @@ pub fn output_to_vec(command: &LineCommand, result: &ResultCode) -> Result<()> {
                 .as_ref()
                 .is_some_and(|a| a.contains(&"-1".to_string()))
             {
-                let _ = write!(buf, "{:?}", token);
+                let _ = write!(buf, "{}\t", token);
             } else {
-                let _ = writeln!(buf, "{:?}", token);
+                let _ = writeln!(buf, "{}", token);
             }
         }
     }
-    if command.file_path.is_some() {
+    if let Some(path_str) = command.file_path.as_ref() {
+        let path = PathBuf::from(path_str);
         fs::write(path, buf)?;
     } else {
-        print!("{:?}", buf);
+        print!("{}", String::from_utf8_lossy(&buf));
     }
-    // this could be print or write
-
     Ok(())
 }
--- src/helpers/parsers.rs
+++ src/helpers/parsers.rs
@@ -1,145 +1,80 @@
-static ESCAPES: [u8; 4] = [b'\\', b'\"', b'`', b' '];
-
 pub fn get_tokens(input: String) -> Vec<String> {
-    let mut tokens: Vec<String> = Vec::new();
-    let mut input_iter = input.trim().bytes().enumerate().peekable();
-    let input_chars: Vec<(usize, u8)> = input_iter.clone().collect();
+    // Improved: respects spaces, quoted substrings, and > operators for redirection
+    let mut tokens = Vec::new();
+    let mut chars = input.trim().chars().peekable();
+    let mut buf = String::new();
+    let mut in_single = false;
+    let mut in_double = false;
 
-    while input_iter.peek().is_some() {
-        let delimiter: u8 = match input_iter.peek().unwrap().1 {
-            b'\'' => {
-                input_iter.next();
-                b'\''
+    while let Some(&ch) = chars.peek() {
+        match ch {
+            '\'' if !in_double => {
+                in_single = !in_single;
+                buf.push(ch);
+                chars.next();
             }
-            b'"' => {
-                input_iter.next();
-                b'"'
+            '"' if !in_single => {
+                in_double = !in_double;
+                buf.push(ch);
+                chars.next();
             }
-            _ => b' ',
-        };
-
-        let parse_internal_quotes = |int: &usize, ch: &u8| -> bool {
-            let mut in_singles_cl = false;
-            let mut in_doubles_cl = false;
-
-            if ch == &b'"' && input_chars[int - 1].1 != b'\\' {
-                in_doubles_cl = !in_doubles_cl;
-            } else if ch == &b'\'' && input_chars[int - 1].1 != b'\\' {
-                in_singles_cl = !in_singles_cl;
+            '>' if !in_single && !in_double => {
+                // Push buffer if any
+                if !buf.trim().is_empty() {
+                    tokens.push(buf.trim().to_string());
+                    buf.clear();
+                }
+                // Consume > and any whitespace
+                chars.next();
+                tokens.push(" > ".trim().to_string());
+                while let Some(' ') = chars.peek() {
+                    chars.next();
+                }
             }
-            if ch == &delimiter && !in_singles_cl && !in_doubles_cl {
-                return delimiter != b' ' && input_chars[int + 1].1 != b' ';
+            ' ' if !in_single && !in_double => {
+                if !buf.is_empty() {
+                    tokens.push(buf.trim().to_string());
+                    buf.clear();
+                }
+                chars.next();
             }
-            true
-        };
-
-        if delimiter == b'\'' {
-            let token_builder = input_iter.by_ref().take_while(|(_a, b)| b != &delimiter);
-            let (_token_int_iter, token_iter): (Vec<usize>, Vec<u8>) = token_builder.unzip();
-            let new_token = String::from_utf8(token_iter).unwrap_or_default();
-            tokens.push(new_token);
-        } else {
-            let token_builder: Vec<(usize, u8)> = input_iter
-                .by_ref()
-                .take_while(|(a, b)| b != &delimiter && parse_internal_quotes(a, b))
-                .collect();
-            let token_iter = handle_escapes(token_builder);
-            let new_token = String::from_utf8(token_iter).unwrap_or_default();
-            tokens.push(new_token);
+            _ => {
+                buf.push(ch);
+                chars.next();
+            }
         }
+    }
+    if !buf.trim().is_empty() {
+        tokens.push(buf.trim().to_string());
     }
     tokens
 }
 
-fn handle_escapes(input: Vec<(usize, u8)>) -> Vec<u8> {
-    let mut output: Vec<u8> = Vec::new();
-    for in_char in &input {
-        if in_char.1 == b'\\' && ESCAPES.contains(&input[in_char.0 + 1].1) {
-            break;
-        } else {
-            output.push(in_char.1);
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_tokenize_with_redirect() {
+        let line = "echo hello > file.txt".to_string();
+        let tokens = get_tokens(line);
+        assert_eq!(tokens, vec!["echo", "hello", ">", "file.txt"]);
     }
-    output
+    #[test]
+    fn test_tokenize_with_spaces_and_quotes() {
+        let line = "echo \"a > b\" > out.txt".to_string();
+        let tokens = get_tokens(line);
+        assert_eq!(tokens, vec!["echo", "\"a > b\"", ">", "out.txt"]);
+    }
+    #[test]
+    fn test_tokenize_multiple_spaces() {
+        let line = "ls    -l    >     result.txt".to_string();
+        let tokens = get_tokens(line);
+        assert_eq!(tokens, vec!["ls", "-l", ">", "result.txt"]);
+    }
+    #[test]
+    fn test_tokenize_redirect_last_wins() {
+        let line = "echo foo > one.txt > two.txt".to_string();
+        let tokens = get_tokens(line);
+        assert_eq!(tokens, vec!["echo", "foo", ">", "one.txt", ">", "two.txt"]);
+    }
 }
-// fn parse_escapes(inp: )
-
-// pub fn parse_comm(inp: &str) -> Vec<String> {
-//     let mut bar = inp.chars().peekable();
-//     let mut retvec: Vec<String> = Vec::new();
-//     let mut fin = false;
-//     let mut addword = false;
-//
-//     while !fin {
-//         let mut retstr = String::new();
-//         let mut withinquotes = false;
-//         let mut withindoublequotes = false;
-//
-//         if addword {
-//             retstr = retvec.pop().unwrap_or_default();
-//             addword = false;
-//         }
-//
-//         while bar.peek().is_some_and(|x| x.is_whitespace()) {
-//             bar.next();
-//         }
-//
-//         let ca: char = match bar.peek() {
-//             Some('\'') => {
-//                 bar.next();
-//                 withinquotes = true;
-//                 '\''
-//             }
-//             Some('"') => {
-//                 bar.next();
-//                 withindoublequotes = true;
-//                 '"'
-//             }
-//             _ => ' ',
-//         };
-//
-//         'wordloop: while bar.peek().is_some() {
-//             let a: char = bar.next().unwrap();
-//             if a == '\'' && !withindoublequotes {
-//                 withinquotes = !withinquotes;
-//             }
-//             if a == '"' && !withinquotes {
-//                 withindoublequotes = !withindoublequotes;
-//             }
-//             if a == '\\' {
-//                 if withinquotes {
-//                     retstr.push(a);
-//                 } else if withindoublequotes {
-//                     if bar.peek().is_some_and(|x| ESCAPES.contains(x)) {
-//                         retstr.push(bar.next().unwrap());
-//                     } else {
-//                         retstr.push(a);
-//                     }
-//                 } else if bar.peek().is_some() {
-//                     retstr.push(bar.next().unwrap());
-//                 } else {
-//                     retstr.push(a);
-//                 }
-//             } else if a == ca {
-//                 if withinquotes {
-//                     retstr.push(a);
-//                 } else if bar.peek().is_some_and(|x| !x.is_whitespace()) && ca != ' ' {
-//                     addword = true;
-//                     break 'wordloop;
-//                 } else if bar.peek().is_some_and(|x| x == &'/' || x == &'"') {
-//                     bar.next();
-//                     break 'wordloop;
-//                 } else {
-//                     break 'wordloop;
-//                 }
-//             } else if ca == ' ' && (a == '"' || a == '\'') {
-//                 bar.next();
-//             } else {
-//                 retstr.push(a);
-//             }
-//         }
-//         retvec.push(retstr);
-//         fin = bar.size_hint().1.unwrap_or(0) == 0;
-//     }
-//     retvec
-// }
